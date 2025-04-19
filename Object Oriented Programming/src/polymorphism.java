dfca1522de8dc65f17e7ea8f30432ec1377b5f3b
Sumit Mukharjee <sumit1mukharjee@gmail.com>
2025-04-19 06:54:22 +0530

    Polymorphism and Inheritence

--- Object Oriented Programming/src/polymorphism.java
+++ Object Oriented Programming/src/polymorphism.java
@@ -1,2 +1,24 @@
+class Students{
+    String name;
+    int age;
+
+    public void printInfo(String name){
+        System.out.println(this.name);
+    }
+    public void printInfo(String name, int age){
+        System.out.println(this.name+" "+this.age);
+    }
+    public void printInfo(int age){
+        System.out.println(this.age);
+    }
+}
 public class polymorphism {
+    public static void main(String[] args) {
+        Students s1 = new Students();
+        s1.name = "john";
+        s1.age = 23;
+
+        s1.printInfo(s1.name, s1.age);
+        s1.printInfo(s1.age);
+    }
 }
